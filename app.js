dad17d8f911b33bf13281ae66019302ff87e6a8a
Karan Kumar <kr123561@gmail.com>
2024-04-07 13:04:44 +0530

    setting up

---
+++ app.js
@@ -0,0 +1,29 @@
+const express =require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+        console.log("database connected");
+    })
+.catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL); 
+}
+
+
+app.get("/",(req,res)=>{
+    res.send("hi, i am root");
+})
+
+
+
+app.listen(8080,()=>{
+    console.log("server is listening to 8080");
+})
