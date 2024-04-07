739e2f48f80b010116cb0216c8b4bae8409a46d4
Karan Kumar <kr123561@gmail.com>
2024-04-07 13:09:41 +0530

    added comment

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express =require("express");
-const app=express();
-const mongoose=require("mongoose");
+const express =require("express");//6
+const app=express();//6
+const mongoose=require("mongoose");//6
 
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";//9.1
 
-main()
+main()//9.3
 .then(()=>{
         console.log("database connected");
     })
@@ -13,17 +13,17 @@ main()
     console.log(err);
 });
 
-async function main(){
+async function main(){//9.2
     await mongoose.connect(MONGO_URL); 
 }
 
 
-app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{//7
     res.send("hi, i am root");
 })
 
 
 
-app.listen(8080,()=>{
+app.listen(8080,()=>{//6
     console.log("server is listening to 8080");
 })
