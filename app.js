8d31963d3bd7f089e7472582634d938968811a6d
Karan Kumar <kr123561@gmail.com>
2024-04-27 12:15:09 +0530

    created apis for CRUD operations

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express =require("express");//6
 const app=express();//6
 const mongoose=require("mongoose");//6
 const listing=require("./models/listing.js");//13
-
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";//10.1
+const path=require("path");
+const methodOverride = require('method-override');//28
+
+app.set("view engine","ejs");//16
+app.set("views",path.join(__dirname,"views"));//16
+app.use(methodOverride('_method'))//28
+
+app.use(express.urlencoded({extended : true}));
+
 
 main()//10.3
 .then(()=>{
@@ -22,18 +30,63 @@ app.get("/",(req,res)=>{//7
     res.send("hi, i am root");
 })
 
-app.get("/testlisting",async(req,res)=>{
-let samplelisting =new listing ({
-    title:"my new villa",
-    description:"nice place",
-    price:1200,
-    locatrion:"kathmandu",
-    country:"nepal",
-})
-await samplelisting.save();
-console.log("listing was saved");
-res.send("successfull testing")
+
+app.get("/listings",async (req,res)=>{//16.index route
+    const allListings=await listing.find({});
+    res.render("listings/index.ejs",{allListings});
 });
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+   
+});
+
+app.get("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    const Listing =await listing.findById(id);
+    res.render("listings/show.ejs",{Listing});
+   
+});
+
+app.post("/listings",async(req,res)=>{
+const newListing=new listing(req.body.listing);
+await newListing.save();
+res.redirect("/listings");
+
+});
+
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    const Listing =await listing.findById(id);
+    res.render("listings/edit.ejs",{Listing})
+});
+
+app.put("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    await listing.findByIdAndUpdate(id,{...req.body.listing})
+    res.redirect(`/listings/${id}`)
+});
+
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+
+
+// app.get("/testlisting",async(req,res)=>{
+// let samplelisting =new listing ({
+//     title:"my new villa",
+//     description:"nice place",
+//     price:1200,
+//     locatrion:"kathmandu",
+//     country:"nepal",
+// })
+// await samplelisting.save();
+// console.log("listing was saved");
+// res.send("successfull testing")
+// });
 
 
 app.listen(8080,()=>{//6
